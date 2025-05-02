1ef57971bd53b6d51a4d6d0bd7150f9c68e806e9
Tanveer Shahriar Arnob <tanveershahriara@gmail.com>
2025-05-02 06:53:18 +0600

    sha-256 hash generated

--- blockchain.cpp
+++ blockchain.cpp
@@ -17,12 +17,24 @@ public:
     Block(string data, string previous_hash) {
         this->data = data;
         this->timestamp = time(0);
-        this->hash = calculate_hash();
         this->previous_hash = previous_hash;
+        this->hash = calculate_hash();
     }
 
+private:
     string calculate_hash() {
-        return "hash";
+        string to_hash = data + previous_hash;
+        unsigned char hash_bytes[SHA256_DIGEST_LENGTH];
+
+        SHA256(reinterpret_cast<const unsigned char*>(to_hash.c_str()), to_hash.size(), hash_bytes);
+
+        char hex_str[SHA256_DIGEST_LENGTH * 2 + 1];
+        for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+            sprintf(hex_str + (i * 2), "%02x", hash_bytes[i]);
+        }
+        hex_str[SHA256_DIGEST_LENGTH * 2] = '\0';
+
+        return string(hex_str);
     }
 };
 
